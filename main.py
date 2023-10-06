4493b9900c21350eca219712e1b65ddcbe1a38a5
Leology0 <leology42@gmail.com>
2023-10-06 13:09:41 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import math
+nums = [10, 30, 40, 45, 70, 80, 85, 90, 100]
+words = ["at", "ball", "cat", "dog", "eye", "fish", "good"]
+unsorted = [30, 20, 70, 40, 50, 100, 90]
+
+# Linear Search Funtion
+def linearSearch(a_list, item):
+    for i in range(len(a_list)):
+        if a_list[i] == item:
+            return i
+    return -1
+
+# Binary search function
+def binarySearch(a_list, item):
+    lower_index = 0
+    higher_index = len(a_list) - 1
+    middle_index = math.floor(lower_index + higher_index / 2)
+    done = False
+
+    while done == False:
+        if item == a_list[middle_index]:
+            return middle_index
+            done = True
+        elif item < a_list[middle_index]:
+            higher_index = middle_index - 1
+        elif item > a_list[middle_index]:
+            lower_index = middle_index + 1
+    
+    return -1
+
+    
+
+def main():
+    print(binarySearch(nums, 100))
+    print(binarySearch(nums, 75))
+    print(binarySearch(words, "fish"))
+    print(binarySearch(words, "at"))
+    print(binarySearch(unsorted, 70))
+
+main()
