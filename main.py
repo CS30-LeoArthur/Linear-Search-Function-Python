37b14796a6ccd7e8acc6c2829d8c099d265270ed
Leology0 <leology42@gmail.com>
2023-10-12 13:08:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def linearSearch(a_list, item):
 def binarySearch(a_list, item):
     lower_index = 0
     higher_index = len(a_list) - 1
-
-    while True:
+    done = False
+    while not done:
         middle_index = math.floor((lower_index + higher_index) / 2)
         if item == a_list[middle_index]:
+            done = True
             return middle_index
         elif item < a_list[middle_index]:
             higher_index = middle_index - 1
